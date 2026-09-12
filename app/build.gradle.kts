fcc5f6014722403d54e6c5b79571491ac758775b
fbvlink2026-lab <sunato2026@gmail.com>
2026-09-13 00:07:19 +0800

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.martodosko.studio"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10068       // ✅ v1.0.55 — PINAKABAGO!
-        versionName = "1.0.68"    // ✅ v1.0.55 — PINAKABAGO!
+        versionCode = 10057       // ✅ v1.0.55 — PINAKABAGO!
+        versionName = "1.0.57"    // ✅ v1.0.55 — PINAKABAGO!
 
         // ⏸️ IKOMENTO MUNA — WALANG C++ PA!
         // externalNativeBuild {
